015511c79a82a996cb9f38664b1fe69760139e1d
YuriyYurchenko-tech <YurchenkoYuriyYu@yandex.ru>
2025-04-07 12:43:03 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       }
     ],
